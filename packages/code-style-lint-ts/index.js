8e186cbef82787e960f26fa18413111af4d0732c
laoer536 <1547698569@qq.com>
2023-02-06 22:56:29 +0800

    fix: env delete

--- packages/code-style-lint-ts/index.js
+++ packages/code-style-lint-ts/index.js
@@ -1,8 +1,4 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
-    },
     "extends": [
         "eslint:recommended",
         "plugin:@typescript-eslint/recommended",
