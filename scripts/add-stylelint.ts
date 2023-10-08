08f896945c0380238df69c9b37785a69b34a6cb3
LJie <1547698569@qq.com>
2023-10-08 17:44:15 +0800

    feat: add stylelint finish

--- scripts/add-stylelint.ts
+++ scripts/add-stylelint.ts
@@ -1,11 +1,35 @@
 import { isPackageExists } from "local-pkg";
 import { execSync } from "child_process";
-import { existsSync } from "node:fs";
+import { existsSync, writeFileSync } from "node:fs";
+import { readFileSync } from "fs";
 
 enum LockFiles {
   "yarn" = "yarn.lock",
   "pnpm" = "pnpm-lock.yaml",
 }
+
+const stylelintConfig = {
+  vue: `{
+  "extends": [
+    "stylelint-config-standard-scss",
+    "stylelint-config-recommended-vue/scss",
+    "stylelint-prettier/recommended"
+  ],
+  "plugins": ["stylelint-order"],
+  "rules": {
+    "order/order": ["custom-properties", "dollar-variables", "declarations", "rules", "at-rules"],
+    "order/properties-alphabetical-order": true
+  }
+}`,
+  other: `{
+  "extends": ["stylelint-config-standard-scss", "stylelint-prettier/recommended"],
+  "plugins": ["stylelint-order"],
+  "rules": {
+    "order/order": ["custom-properties", "dollar-variables", "declarations", "rules", "at-rules"],
+    "order/properties-alphabetical-order": true
+  }
+}`,
+};
 
 function getPackageManager() {
   if (existsSync(LockFiles.yarn)) {
@@ -26,13 +50,39 @@ function getStyleFileType() {
     return "css";
   }
 }
-
 function run() {
-  const packageManager = getPackageManager();
   const styleFileType = getStyleFileType();
-  if (isPackageExists("vue")) {
-    execSync(
-      `${packageManager} stylelint stylelint-config-recommended-vue stylelint-config-standard-${styleFileType} stylelint-order stylelint-prettier`,
-    );
+  const packageManager = getPackageManager();
+  if (styleFileType !== "scss") {
+    throw new Error("Currently, only SCSS is supported.");
   }
+  const workspacesCommand = () => {
+    const isWorkspacesMode =
+      Boolean(JSON.parse(readFileSync("package.json", "utf-8")).workspaces) || existsSync("pnpm-lock.yaml");
+    if (isWorkspacesMode) {
+      if (packageManager === "pnpm") {
+        return "-w";
+      } else if (packageManager === "yarn") {
+        return "-W";
+      } else {
+        return "";
+      }
+    } else {
+      return "";
+    }
+  };
+  const install = packageManager === "npm" ? "install" : "add";
+  const isVue = isPackageExists("vue");
+  if (!isPackageExists("prettier")) {
+    execSync(`${packageManager} ${install} prettier -D ${workspacesCommand()}`);
+  }
+  execSync(
+    `${packageManager} ${install} stylelint stylelint-config-standard-${styleFileType} stylelint-prettier stylelint-order -D ${workspacesCommand()}`,
+  );
+  if (isVue) {
+    execSync(`${packageManager} ${install} stylelint-config-recommended-vue postcss-html -D ${workspacesCommand()}`);
+  }
+  writeFileSync(".stylelintrc", stylelintConfig[isVue ? "vue" : "other"]);
 }
+
+run();
